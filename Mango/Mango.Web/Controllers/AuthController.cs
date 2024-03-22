a2ff672660626fc404b9b36744361698dbfa6de8
Rin <tuannmctk42@gmail.com>
2024-03-22 07:21:59 +0700

    add AuthController

--- Mango/Mango.Web/Controllers/AuthController.cs
+++ Mango/Mango.Web/Controllers/AuthController.cs
@@ -1,11 +1,73 @@
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mango.Web.Controllers;
 
 public class AuthController : Controller
 {
-    // GET
+    private readonly IAuthService _authService;
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost]
     public IActionResult Login()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Register()
+    {
+        var roleList = new List<SelectListItem>()
+        {
+            new SelectListItem{Text=SD.RoleAdmin,Value=SD.RoleAdmin},
+            new SelectListItem{Text=SD.RoleCustomer,Value=SD.RoleCustomer},
+        };
+
+        ViewBag.RoleList = roleList;
+        return View();
+    }
+    
+    
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationRequestDto obj)
+    {
+        ResponseDto result = await _authService.RegisterAsync(obj);
+        ResponseDto assingRole;
+
+        if(result!=null && result.IsSuccess)
+        {
+            if (string.IsNullOrEmpty(obj.Role))
+            {
+                obj.Role = SD.RoleCustomer;
+            }
+            assingRole = await _authService.AssignRoleAsync(obj);
+            if (assingRole!=null && assingRole.IsSuccess)
+            {
+                TempData["success"] = "Registration Successful";
+                return RedirectToAction(nameof(Login));
+            }
+        }
+        else
+        {
+            TempData["error"] = result.Message;
+        }
+
+        var roleList = new List<SelectListItem>()
+        {
+            new SelectListItem{Text=SD.RoleAdmin,Value=SD.RoleAdmin},
+            new SelectListItem{Text=SD.RoleCustomer,Value=SD.RoleCustomer},
+        };
+
+        ViewBag.RoleList = roleList;
+        return View(obj);
+    }
+
+    public IActionResult Logout()
     {
         return View();
     }
